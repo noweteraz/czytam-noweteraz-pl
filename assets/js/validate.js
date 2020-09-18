395afa378b7b5a3d31da14f87735807208f53fb5
axel <admin@bf1.pl>
2020-09-18 16:08:42 +0200

    Update.

---
+++ assets/js/validate.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+
+  $('#form-submit').click(function(e) {
+
+    $("#contactForm").validate({
+      ignore: ':hidden:not(.captcha-control)',
+        rules: {
+            'entry.122701191': {
+                required: true
+            },
+            emailAddress: {
+                required: true,
+                email: true
+            },
+            'entry.560386344': {
+                required: true
+            },
+            reCaptchaControl: {
+                required: function () {
+                    if (grecaptcha.getResponse() == '') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }  
+        },
+        messages: {
+            'entry.122701191': {
+                required: "Proszę wpisać Imię"
+            },
+            emailAddress: {
+                required: "Proszę wpisać Email",
+                email: "Musisz wpisać email w formacie nazwa@domena"
+            },
+            'entry.560386344': {
+                required: "Proszę napisać wiadomość"
+            },
+            reCaptchaControl: {
+                required: "Proszę zaznaczyć Nie jestem robotem"
+            }
+        },
+        submitHandler: function(form) {
+                form.submit();
+        }
+    });
+    
+            });
+
+});
+
+$(document).ready(function() {
+
+    $('#form-submit-project').click(function(e) {
+  
+      $("#submitForm").validate({
+        ignore: ':hidden:not(.captcha-control)',
+          rules: {
+              'entry.1730560742': {
+                  required: true
+              },
+              emailAddress: {
+                  required: true,
+                  email: true
+              },
+              'entry.174369693': {
+                  required: true
+              },
+              'entry.209624869': {
+                required: true
+            },
+              reCaptchaControl: {
+                  required: function () {
+                      if (grecaptcha.getResponse() == '') {
+                          return true;
+                      } else {
+                          return false;
+                      }
+                  }
+              }  
+          },
+          messages: {
+              'entry.1730560742': {
+                  required: "Proszę wpisać Imię i Nazwisko"
+              },
+              emailAddress: {
+                  required: "Proszę wpisać Email",
+                  email: "Musisz wpisać email w formacie nazwa@domena"
+              },
+              'entry.174369693': {
+                required: "Proszę wpisać nazwę instytucji"
+            },
+            'entry.209624869': {
+                  required: "Proszę wpisać miejscowość"
+              },
+              reCaptchaControl: {
+                  required: "Proszę zaznaczyć Nie jestem robotem"
+              }
+          },
+          submitHandler: function(form) {
+                  form.submit();
+          }
+      });
+      
+              });
+  
+  });
---
+++ index.md
@@ -0,0 +1,6 @@
+---
+title: Projekt Wiem, bo czytam
+description: Projekt zapobieganie uzależnieniom i przyczynom przestępczości wśród dorosłych, dzieci i młodzieży.  
+layout: home
+lang: pl_PL
+---
